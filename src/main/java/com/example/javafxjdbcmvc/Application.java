0a6b9308887b904d3f9260c6af6810148c8e1487
Juan Faria <juanfariacrf@gmail.com>
2023-05-06 16:12:04 -0300

    Create Main View Designer

--- src/main/java/com/example/javafxjdbcmvc/Application.java
+++ src/main/java/com/example/javafxjdbcmvc/Application.java
@@ -2,6 +2,7 @@ package com.example.javafxjdbcmvc;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,7 +11,10 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        ScrollPane scrollPane = fxmlLoader.load();//Usado para instanciar o objeto correto que será usado
+        scrollPane.setFitToHeight(true);//Feito para deixar padrão a altura
+        scrollPane.setFitToWidth(true); //Feito para deixar padrão a largura 
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Sample JavaFx Application");
         stage.setScene(scene);
         stage.show();
